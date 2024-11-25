90f34a4c27e985dd55c6dcf08c74bc32660a6443
juan ignacio <nho13@hotmail.com>
2024-11-25 16:11:07 +0100

    feat: initial commit

---
+++ src/main/java/com/vetpet/VetPet/VetPetApplication.java
@@ -0,0 +1,13 @@
+package com.vetpet.VetPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetPetApplication.class, args);
+	}
+
+}
