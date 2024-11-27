5f425cad1dcbbb503540440d193d029dfb829477
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-11-27 09:51:02 +0100

    feat: TutorController --getTutorById

--- src/main/java/com/vetpet/VetPet/controllers/TutorController.java
+++ src/main/java/com/vetpet/VetPet/controllers/TutorController.java
@@ -2,14 +2,18 @@ package com.vetpet.VetPet.controllers;
 
 import com.vetpet.VetPet.Tutor;
 import com.vetpet.VetPet.repository.TutorRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
- @RestController
- @RequestMapping("/api/tutors")
+@RestController
+@RequestMapping("/api/tutors")
 
 public class TutorController {
     private final TutorRepository TUTOR_REPOSITORY;
@@ -18,8 +22,19 @@ public class TutorController {
         TUTOR_REPOSITORY = tutorRepository;
 
     }
+
     @GetMapping
-    public List<Tutor> GetTutorsList(){
+    public List<Tutor> GetTutorsList() {
         return TUTOR_REPOSITORY.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTutorById(@PathVariable Long id){
+        Optional<Tutor> optionalTutor = TUTOR_REPOSITORY.findById(id);
+        if(optionalTutor.isEmpty()){
+            return new ResponseEntity<>("El tutor con " + id +" actualmente no se encuentra asociado a un tutor registrado", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(optionalTutor.get(), HttpStatus.OK);
+    }
 }
+
