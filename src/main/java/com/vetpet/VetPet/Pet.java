b9e3a11a3c136eb2fef8f16475a1b42f689450cc
juan ignacio <nho13@hotmail.com>
2024-11-28 16:12:51 +0100

    feat: class pet filled

--- src/main/java/com/vetpet/VetPet/Pet.java
+++ src/main/java/com/vetpet/VetPet/Pet.java
@@ -1,4 +1,73 @@
 package com.vetpet.VetPet;
 
+
+import jakarta.persistence.*;
+
+@Entity
+
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String breed;
+    private String class_species;
+
+    public Pet() {
+    }
+
+    public Pet(String name, int age, String breed, String class_species) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.class_species = class_species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getClass_species() {
+        return class_species;
+    }
+
+    public void setClass_species(String class_species) {
+        this.class_species = class_species;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private Pet pet;
+
+
 }
