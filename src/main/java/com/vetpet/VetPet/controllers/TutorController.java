0657975561eab3134f8f34b442fc49943a58ade5
PCalvoGarcia <paulacalvogarcia@gmail.com>
2024-11-28 12:17:20 +0100

    feat: TutorController --DeleteTutorById; Close #26 ++ test: DeleteTutorByIdTest -- test delete and check error management delete; Close #28

--- src/main/java/com/vetpet/VetPet/controllers/TutorController.java
+++ src/main/java/com/vetpet/VetPet/controllers/TutorController.java
@@ -43,20 +43,31 @@ public class TutorController {
         Tutor newTutor = TUTOR_REPOSITORY.save(tutor);
         return new ResponseEntity<>(newTutor,HttpStatus.CREATED);
     }
-@PutMapping("/{id}")
-    public ResponseEntity<?>updateTutorId(@PathVariable Long id,@RequestBody Tutor updateTutor) {
-       Optional<Tutor> optionalTutor = TUTOR_REPOSITORY.findById(id);
-       if (optionalTutor.isEmpty()) {
-           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-       }
-       Tutor basetutor = optionalTutor.get();
-       basetutor.setName(updateTutor.getName());
-       basetutor.setSurname(updateTutor.getSurname());
-       basetutor.setPhoneNumber(updateTutor.getPhoneNumber());
-    TUTOR_REPOSITORY.save(basetutor);
-    return new ResponseEntity<>(" Tutor has been updated.",HttpStatus.OK);
 
-}
+    @PutMapping("/{id}")
+        public ResponseEntity<?> updateTutorById(@PathVariable Long id, @RequestBody Tutor updateTutor) {
+           Optional<Tutor> optionalTutor = TUTOR_REPOSITORY.findById(id);
+           if (optionalTutor.isEmpty()) {
+               return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+           }
+           Tutor basetutor = optionalTutor.get();
+           basetutor.setName(updateTutor.getName());
+           basetutor.setSurname(updateTutor.getSurname());
+           basetutor.setPhoneNumber(updateTutor.getPhoneNumber());
+        TUTOR_REPOSITORY.save(basetutor);
+        return new ResponseEntity<>(" Tutor has been updated.",HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+        public ResponseEntity<?> deleteTutorById (@PathVariable Long id){
+        Optional<Tutor> optionalTutor = TUTOR_REPOSITORY.findById(id);
+        if (optionalTutor.isEmpty()) {
+            return new ResponseEntity<>("The id " + id + " isn´t associated with any tutor currently.",HttpStatus.NOT_FOUND);
+        }
+        TUTOR_REPOSITORY.deleteById(id);
+        return new ResponseEntity<>(" Tutor has been deleted.",HttpStatus.OK);
+
+        }
 
 
 }
---
+++ src/test/java/com/vetpet/VetPet/controllers/tutoracceptancetest/DeleteTutorByIdTest.java
@@ -0,0 +1,50 @@
+package com.vetpet.VetPet.controllers.tutoracceptancetest;
+
+import com.vetpet.VetPet.Tutor;
+import com.vetpet.VetPet.repository.TutorRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DeleteTutorByIdTest {
+    @Autowired
+    private TutorRepository tutorRepository;
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void given1Tutor_whenDeleteTutorById_thenReturnOK() throws Exception {
+        //given
+        Tutor tutor1 = new Tutor("Evelyn","Quevedo",12345678);
+        tutorRepository.save(tutor1);
+        //when & then
+        mockMvc.perform(delete("/api/tutors/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string(        " Tutor has been deleted."
+                ));// Mensaje esperado.
+    }
+
+    @Test
+    void givenNonExistingTutor_whenGetTutorById_thenReturnNotFound() throws Exception {
+        //when & then
+        mockMvc.perform(get("/api/tutors/1") // ID que no existe.
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound()) // Código de estado 404.
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string("El tutor con 1 actualmente no se encuentra asociado a un tutor registrado")); // Mensaje esperado.
+    }
+}
